2e3bb61ca3e28d506173b0c04aedcf6b5d2aa330
Sanjay Bhangar <sanjay@mapbox.com>
2015-11-10 17:35:46 +0530

    working test with big buck bunny

--- index.js
+++ index.js
@@ -23,7 +23,9 @@
         // });
         var videoSource = new mapboxgl.VideoSource({
             'urls': [
-                "big-buck-bunny_trailer.webm"
+                // "big-buck-bunny_trailer.webm"
+                'https://video970.pad.ma/WX/240p1.webm'
+                // "https://upload.wikimedia.org/wikipedia/commons/c/c0/Big_Buck_Bunny_4K.webm"
             ],
            'coordinates': [
                [-76.54335737228394, 39.18579907229748],
